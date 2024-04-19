eea871e093a208efac0809989dd07d62ca35e497
Andres Trujillo <atrujillo@Andres-Trujillos-Mac.local>
2024-04-19 10:51:47 -0400

    framer 3d test success

--- src/pages/_components/project/project.tsx
+++ src/pages/_components/project/project.tsx
@@ -3,13 +3,13 @@ import type {
   ProjectFeature,
   ProjectHero,
 } from "@/types";
-// import type { ReactElement, type ReactNode } from "react";
 import {
   motion,
   useScroll,
   useTransform,
   useSpring,
   MotionValue,
+  useTime,
 } from "framer-motion";
 import {
   Card,
@@ -30,6 +30,17 @@ import { MovingBorderWrapper } from "@/components/ui/moving-border";
 
 import BHFHero from "@/pages/_assets/bhf-hero.png";
 import { projects } from "@/config/projects";
+
+// import * as THREE from "three";
+
+import {
+  Canvas,
+  useThree,
+  useFrame,
+} from "@react-three/fiber";
+import { useRef, useLayoutEffect } from "react";
+
+import { degreesToRadians, progress, mix } from "popmotion";
 
 const IMG_PADDING = 12;
 
@@ -122,6 +133,7 @@ function Project(props: Project) {
             moveSelectedFeatureToTop
           }
         />
+
         <ProjectTools tools={tools} />
       </ProjectGrid>
     </ProjectContainer>
@@ -261,7 +273,7 @@ function ProjectHero({
 }) {
   // console.log(activeFeature.content);
   return (
-    <motion.div className="relative w-full col-span-9 row-span-12 ">
+    <div className="relative w-full col-span-9 row-span-12 ">
       {/* <CardGlow /> */}
       <Card
         className={cn(
@@ -276,7 +288,7 @@ function ProjectHero({
         {/* <CardGlow /> */}
         <CardContent>{children}</CardContent>
       </Card>
-    </motion.div>
+    </div>
   );
 }
 
@@ -322,9 +334,10 @@ function ProjectFeatures({
 
   function FadeInCard() {
     return (
-      <motion.div className="relative w-full h-full">
+      <div className="relative w-full h-full">
         {features.map((feature, idx) => (
           <motion.div
+            layout
             key={feature.title}
             layoutId={feature.title}
             style={{
@@ -369,18 +382,17 @@ function ProjectFeatures({
             </Card>
           </motion.div>
         ))}
-      </motion.div>
+      </div>
     );
   }
-  console.log(cards);
 
   return (
-    <motion.div className="relative col-span-3 row-span-6 group isolate">
+    <div className="relative col-span-3 row-span-6 group isolate">
       <CardGlow />
       {/* <Card className="relative "> */}
       {cards.map((card, index) => (
         <motion.div
-          layout
+          // layout
           // layoutId={card.title}
           key={card.title}
           className="absolute  h-full w-full  rounded-3xl   shadow-xl   border-white-[0.1]   shadow-white-[0.05] flex flex-col justify-between"
@@ -397,7 +409,7 @@ function ProjectFeatures({
         </motion.div>
       ))}
       {/* </Card> */}
-    </motion.div>
+    </div>
   );
 }
 
@@ -417,6 +429,83 @@ const ProjectNavigator = React.forwardRef<
 ProjectNavigator.displayName = "ProjectNavigator";
 
 function ProjectTools({ tools }: { tools: string[] }) {
+  const color = "#111111";
+
+  const Icosahedron = () => (
+    <mesh rotation-x={0.35}>
+      <icosahedronGeometry args={[1, 0]} />
+      <meshBasicMaterial wireframe color={color} />
+    </mesh>
+  );
+  const Star = ({ p }: { p: number }) => {
+    // const ref = useRef<THREE.Object3D>(null);
+    const ref = useRef(null);
+
+    useLayoutEffect(() => {
+      const distance = mix(2, 3.5, Math.random());
+      const yAngle = mix(
+        degreesToRadians(80),
+        degreesToRadians(100),
+        Math.random()
+      );
+      const xAngle = degreesToRadians(360) * p;
+      ref.current!.position.setFromSphericalCoords(
+        distance,
+        yAngle,
+        xAngle
+      );
+    });
+
+    return (
+      <mesh ref={ref}>
+        <boxGeometry args={[0.05, 0.05, 0.05]} />
+        <meshBasicMaterial wireframe color={color} />
+      </mesh>
+    );
+  };
+
+  function Scene({ numStars = 100 }) {
+    const gl = useThree((state) => state.gl);
+    const { scrollYProgress } = useScroll();
+    const yAngle = useTransform(
+      scrollYProgress,
+      [0, 1],
+      [0.001, degreesToRadians(180)]
+    );
+    const distance = useTransform(
+      scrollYProgress,
+      [0, 1],
+      [10, 3]
+    );
+    const time = useTime();
+
+    useFrame(({ camera }) => {
+      camera.position.setFromSphericalCoords(
+        distance.get(),
+        yAngle.get(),
+        time.get() * 0.0005
+      );
+      camera.updateProjectionMatrix();
+      camera.lookAt(0, 0, 0);
+    });
+
+    useLayoutEffect(() => gl.setPixelRatio(0.3));
+
+    const stars = [];
+    for (let i = 0; i < numStars; i++) {
+      stars.push(
+        <Star key={i} p={progress(0, numStars, i)} />
+      );
+    }
+
+    return (
+      <>
+        <Icosahedron />
+        {stars}
+      </>
+    );
+  }
+
   return (
     <Card className="col-span-3 row-span-6">
       <CardHeader>
@@ -426,6 +515,11 @@ function ProjectTools({ tools }: { tools: string[] }) {
         {tools.map((tool, index) => (
           <span key={index}>{tool}</span>
         ))}
+        <div className="container">
+          <Canvas gl={{ antialias: false }}>
+            <Scene />
+          </Canvas>
+        </div>
       </CardContent>
     </Card>
   );
