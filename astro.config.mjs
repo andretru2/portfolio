dd0e69a46026d5371407fcfde3f1f3495f458ea4
Andres Trujillo <atrujillo@Andres-Trujillos-Mac.local>
2024-04-30 18:04:48 -0400

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import icon from "astro-icon";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   // ...
-  integrations: [tailwind(), react(), icon()]
+  integrations: [tailwind(), react(), icon()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  }),
+  imageService: true,
 });
