d61f48862be8e475cb3b9ac8f0e470dbb0e9f039
Andres Trujillo <atrujillo@Andres-Trujillos-Mac.local>
2024-06-08 13:42:54 -0400

    new font; other updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,20 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import icon from "astro-icon";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import lenis from "astro-lenis";
 
 // https://astro.build/config
 export default defineConfig({
   // ...
-  integrations: [tailwind(), react(), icon()],
+  integrations: [tailwind(), react(), icon(), lenis()],
   output: "server",
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: true
     },
-    maxDuration: 8,
+    maxDuration: 8
   }),
-  imageService: true,
+  imageService: true
 });
