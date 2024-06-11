0374eb9b50a48d970d1edf4095390f9cd7b74fc1
Andres Trujillo <atrujillo@Andres-Trujillos-Mac.local>
2024-06-11 14:54:40 -0400

    hero parallax working

---
+++ src/pages/_components/hero-parallax.tsx
@@ -0,0 +1,30 @@
+import {
+  useScroll,
+  useTransform,
+  motion,
+} from "framer-motion";
+import { useRef } from "react";
+
+export function HeroParallax({ children }) {
+  const container = useRef();
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end start"],
+  });
+
+  const y = useTransform(
+    scrollYProgress,
+    [0, 1],
+    ["0svh", "100svh"]
+  );
+
+  return (
+    <motion.section
+      ref={container}
+      style={{ y }}
+      className="bg-bgColorHero h-full relative min-h-[115svh] grid place-content-center gap-xl justify-center w-full min-w-7xl [perspective:1000px] overflow-hidden [&>*]:-translate-y-8 "
+    >
+      {children}
+    </motion.section>
+  );
+}
