fb2fe5c9ba4fd17c3391120656813d8801f5446f
Andres Trujillo <atrujillo@Andres-Trujillos-Mac.local>
2024-07-15 11:13:44 -0400

    more responsive changes

--- src/components/ui/stat.tsx
+++ src/components/ui/stat.tsx
@@ -12,18 +12,18 @@ export function Stat({
   includePercentageSymbol,
 }: StatProps) {
   return (
-    <div className="flex flex-col gap-xs items-center">
+    <div className="flex flex-col gap-xs items-center *:text-center">
       {typeof value === "number" ? (
         <Counter
           value={value}
-          className="fluid:text-4xl font-bold text-accent"
+          className="fluid:text-sm sm:fluid:text-4xl font-bold text-accent"
           includePercentageSymbol={includePercentageSymbol}
         />
       ) : (
         <h5 className="text-accent">{value}</h5>
       )}
 
-      <span className="text-s font-light opacity-80">
+      <span className="text-sm sm:text-base font-light opacity-80 ">
         {label}
       </span>
     </div>
